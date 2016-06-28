8e6f3e80dbbe7f3f0a5aaf714925915bcbcdca88
J. Kevin Bay <seregon@gmail.com>
2016-06-28 19:06:19 -0400

    Fleshed out OperatorInterface

--- neoalchemy/schema/operations.py
+++ neoalchemy/schema/operations.py
@@ -3,91 +3,69 @@
 class OperatorInterface(object):
 
     # Mathematical Operators
-    def __add__(self, x):    # self + x
+    def __add__(self, x):         # self + x
         return '%%s.%s + %r' % (self.key, x)
 
-    def __sub__(self, x):
-        pass    # self - x
+    def __radd__(self, x):        # x + self
+        return self.__add__(x)
 
-    def __rsub__(self, x):
-        pass    # x - self
+    def __sub__(self, x):         # self - x
+        return '%%s.%s - %r' % (self.key, x)
 
-    def __mul__(self, x):
-        pass    # self * x
+    def __rsub__(self, x):        # x - self
+        return '%r - %%s.%s' % (x, self.key)
 
-    def __rmul__(self, x):
-        pass    # x * self
+    def __mul__(self, x):         # self * x
+        return '%%s.%s * %r' % (self.key, x)
 
-    def __div__(self, x):
-        pass    # self / x
+    def __rmul__(self, x):        # x * self
+        return self.__mul__(x)
 
-    def __rdiv__(self, x):
-        pass    # x / self
+    def __div__(self, x):         # self / x
+        return '%%s.%s / %r' % (self.key, x)
 
-    def __truediv__(self, x):
-        pass    # self / x  __future__.division
+    def __rdiv__(self, x):        # x / self
+        return '%r / %%s.%s' % (x, self.key)
 
-    def __rtruediv__(self, x):
-        pass    # x / self  __future__.division
+    def __truediv__(self, x):     # self / x  (__future__.division)
+        return self.__div__(x)
 
-    def __floordiv__(self, x):
-        pass    # self // x
+    def __rtruediv__(self, x):    # x / self  (__future__.division)
+        return self.__rdiv__(x)
 
-    def __rfloordiv__(self, x):
-        pass    # x // self
+    def __floordiv__(self, x):    # self // x
+        return self.__div__(x)
 
-    def __mod__(self, x):
-        pass    # self % x
+    def __rfloordiv__(self, x):   # x // self
+        return self.__rdiv__(x)
 
-    def __rmod__(self, x):
-        pass    # x % self
+    def __mod__(self, x):         # self % x
+        return '%%s.%s %% %r' % (self.key, x)
 
-    def __pow__(self, x):
-        pass    # self ** x
+    def __rmod__(self, x):        # x % self
+        return '%r %% %%s.%s' % (x, self.key)
 
-    def __rpow__(self, x):
-        pass    # x ** self
+    def __pow__(self, x):         # self ** x
+        return '%%s.%s ^ %r' % (self.key, x)
+
+    def __rpow__(self, x):        # x ** self
+        return '%r ^ %%s.%s' % (x, self.key)
 
     # Comparison Operators
-    def __eq__(self, x):    # self == x
+    def __eq__(self, x):          # self == x
         return '%%s.%s = %r' % (self.key, x)
 
-    def __ne__(self, x):    # self != x
+    def __ne__(self, x):          # self != x
         return '%%s.%s <> %r' % (self.key, x)
 
-    def __lt__(self, x):    # self < x
+    def __lt__(self, x):          # self < x
         return '%%s.%s < %r' % (self.key, x)
 
-    def __gt__(self, x):    # self > x
+    def __gt__(self, x):          # self > x
         return '%%s.%s > %r' % (self.key, x)
 
-    def __le__(self, x):    # self <= x
+    def __le__(self, x):          # self <= x
         return '%%s.%s <= %r' % (self.key, x)
 
-    def __ge__(self, x):    # self >= x
+    def __ge__(self, x):          # self >= x
         return '%%s.%s >= %r' % (self.key, x)
-
-    # Logical Operators
-    def __and__(self, x):
-        pass    # self & x
-
-    def __rand__(self, x):
-        pass    # x & self
-
-    def __or__(self, x):
-        pass    # self | x
-
-    def __ror__(self, x):
-        pass    # x | self
-
-    def __xor__(self, x):
-        pass    # self ^ x
-
-    def __rxor__(self, x):
-        pass    # x ^ self
-
-    def __bool__(self, x):
-        pass    # bool(x)  Python 3
-
-    def __nonzero__(self, x):
-        pass    # bool(x)  Python 2
